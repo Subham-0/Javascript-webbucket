b22b9af417d813d6c465b0b510e887154ae06a7d
Subham-0 <subhamd2022@gift.edu.in>
2023-11-28 11:59:41 +0000

    Test file added

---
+++ .devcontainer/01-basic/text.js
@@ -0,0 +1,8 @@
+console.log("you are the concorar");
+/*
+1- javascript is a high level scripting language used to devlop web and mobile application efficiency.
+2- we used javascript in clientside as well as serverside programming language.
+3- javascript was developed to write logic/functionality to our code to perform everywhere.
+4- javascript was developed in 1997 and the first version was ES2,ES3.
+5- Now we use ES6 versiion after 2015 ES6 Echma script 6(Vanilla JavaScript ).
+*/
